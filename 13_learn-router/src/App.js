ce08ed2f4e9eb92cffb35aad8fc1d77e77d12db3
CoderJJ <1325964835@qq.com>
2022-09-18 21:34:42 +0800

    react路由初步使用

---
+++ 13_learn-router/src/App.js
@@ -0,0 +1,28 @@
+import React, { PureComponent } from 'react'
+
+import { NavLink,Route,BrowserRouter,Switch} from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+
+export default class App extends PureComponent {
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <NavLink exact to='/' activeStyle={{color:'orange',fontSize:'24px'}}>首页</NavLink>
+          <NavLink to={'/about'} activeStyle={{color:'orange',fontSize:'24px'}}>关于</NavLink>
+          <NavLink to={'/profile'} activeStyle={{color:'orange',fontSize:'24px'}}>我的</NavLink>
+
+          <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path={'/about'} component={About} />
+          <Route path={'/profile'} component={Profile} />
+          </Switch>
+          
+
+        </BrowserRouter>
+      </div>
+    )
+  }
+}
