5b45e0892c7a229269dc90faec77ac0469b3faa6
CoderJJ <1325964835@qq.com>
2022-09-18 22:30:07 +0800

    嵌套路由1

--- 13_learn-router/src/App.js
+++ 13_learn-router/src/App.js
@@ -3,6 +3,7 @@ import React, { PureComponent } from 'react'
 import { NavLink,Route,BrowserRouter,Switch} from 'react-router-dom'
 import About from './pages/About'
 import Home from './pages/Home'
+import NoMatch from './pages/NoMatch'
 import Profile from './pages/Profile'
 
 export default class App extends PureComponent {
@@ -14,10 +15,25 @@ export default class App extends PureComponent {
           <NavLink to={'/about'} activeStyle={{color:'orange',fontSize:'24px'}}>关于</NavLink>
           <NavLink to={'/profile'} activeStyle={{color:'orange',fontSize:'24px'}}>我的</NavLink>
 
-          <Switch>
-          <Route exact path='/' component={Home} />
+          {/* <Route exact path='/' component={Home} />
           <Route path={'/about'} component={About} />
           <Route path={'/profile'} component={Profile} />
+          <Route  component={NoMatch} /> */}
+
+          {/* 
+          // 2.使用switch
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path={'/about'} component={About} />
+            <Route path={'/profile'} component={Profile} />
+            <Route  component={NoMatch} />
+          </Switch> */}
+
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path={'/about'} component={About} />
+            <Route path={'/profile'} component={Profile} />
+            <Route  component={NoMatch} />
           </Switch>
           
 
--- 13_learn-router/src/pages/About.js
+++ 13_learn-router/src/pages/About.js
@@ -1,10 +1,36 @@
 import React, { PureComponent } from 'react'
+import {NavLink,Route,Switch} from 'react-router-dom'
 
+function AboutContact(props){
+  return (
+    <h3>联系我们哦</h3>
+  )
+}
+function AboutMain(props){
+  return (
+    <h3>这里是About主页</h3>
+  )
+}
+function AboutCulture(props){
+  return (
+    <h3>这里是About文化</h3>
+  )
+}
 export default class About extends PureComponent {
   render() {
     return (
       <div>
         <h2>About</h2>
+        {/* 使用嵌套路由 */}
+        <NavLink exact to='/about' activeStyle={{color:'red',fontSize:'20px'}}>企业详情</NavLink>
+        <NavLink exact to={'/about/culture'} activeStyle={{color:'red',fontSize:'20px'}}>企业文化</NavLink>
+        <NavLink exact to={'/about/contact'} activeStyle={{color:'red',fontSize:'20px'}}>联系我们</NavLink>
+
+        <Switch>
+            <Route exact path='/about' component={AboutMain} />
+            <Route path={'/about/culture'} component={AboutCulture} />
+            <Route path={'/about/contact'} component={AboutContact} />
+          </Switch>
       </div>
     )
   }
